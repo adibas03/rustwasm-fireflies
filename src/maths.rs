3dbaca8aebc1c47d692fddb77749f23a461a0e0d
adibas03 <adibas03@ymail.com>
2023-08-29 22:18:53 +0100

    catch up

--- src/maths.rs
+++ src/maths.rs
@@ -3,8 +3,9 @@
 // use rand::prelude::*;
 // use rand::random;
 
-fn randomU8(availableValues: <[u8]>) -> u8 {
+fn random_u8(available_values: &[u8]) -> u8 {
     // let mut rng = rand::thread_rng();
     // let n1: u8 = rng.gen::<u8>();
     let n1: u8 = rand::random::<u8>();
+    return n1
 }
